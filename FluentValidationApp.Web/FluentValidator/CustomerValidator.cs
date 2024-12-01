fe41ede10af38cd0ed4fd593b3cd423a3315ec7e
dogan.dll <dogan.dellal@boyner.com.tr>
2024-12-01 22:48:45 +0300

    FluentValidation Session 1

---
+++ FluentValidationApp.Web/FluentValidator/CustomerValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+
+namespace FluentValidationApp.Web.FluentValidator
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name alanı boş olamaz");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş olamaz").EmailAddress().WithMessage("Email alanı doğru formatta girilmelidir.");
+            RuleFor(x => x.Age).NotEmpty().WithMessage("Age alanı boş olamaz").InclusiveBetween(18, 60).WithMessage("Yaş aralığı 18 - 60 arası olmalıdır !");
+        }
+    }
+}
